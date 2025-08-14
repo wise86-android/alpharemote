e91d3b137ff10c4561bc49a452527d13a8cabadc
Staacks <staacks@physik.rwth-aachen.de>
2025-08-15 00:02:59 +0200

    Fix: Help drawer not showing texts.

--- app/src/main/java/org/staacks/alpharemote/ui/help/HelpDialogViewModel.kt
+++ app/src/main/java/org/staacks/alpharemote/ui/help/HelpDialogViewModel.kt
@@ -1,8 +1,9 @@
 package org.staacks.alpharemote.ui.help
 
 import androidx.lifecycle.ViewModel
+import org.staacks.alpharemote.R
 
 class HelpDialogViewModel: ViewModel() {
-    var titleId: Int? = null
-    var textId: Int? = null
+    var titleId: Int = R.id.help_title
+    var textId: Int = R.id.help_text
 }
