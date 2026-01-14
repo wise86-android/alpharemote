a93d6caaf903a41d4a6195e1c7211ff71b162a76
wise <wise86android@gmail.com>
2026-01-14 22:18:54 +0100

    feat: improve doc about device scanner

--- app/src/main/java/org/staacks/alpharemote/ui/settings/CompanionDeviceHelper.kt
+++ app/src/main/java/org/staacks/alpharemote/ui/settings/CompanionDeviceHelper.kt
@@ -20,7 +20,8 @@ import org.staacks.alpharemote.utils.hasBluetoothPermission
 object CompanionDeviceHelper {
 
     fun getAssociation(context: Context): List<String> {
-        val deviceManager = context.getSystemService(Context.COMPANION_DEVICE_SERVICE) as CompanionDeviceManager
+        val deviceManager = ContextCompat.getSystemService(context, CompanionDeviceManager::class.java) ?: return emptyList()
+
         return deviceManager.associations
     }
 
@@ -28,28 +29,22 @@ object CompanionDeviceHelper {
         val deviceFilter: BluetoothLeDeviceFilter = BluetoothLeDeviceFilter.Builder()
             .setScanFilter(ScanFilter.Builder()
                 .setManufacturerData(
-                    0x012d,
-                    byteArrayOf(            //Filter raw values
-                        0x03.toByte(), 0x00.toByte(),               //Camera
-                        0x64.toByte(),                              //Protocol version
-                        0x00.toByte(),                              //Unknown
-                        0x00.toByte(), 0x00.toByte(),               //Model
-                        0x22.toByte(), 0x40.toByte(), 0x00.toByte() //Camera state, bit 0x40 indicates ready to pair
+                    0x012d,                           // Sony manufacturedId
+                    byteArrayOf(                    // Filter raw values
+                        0x03.toByte(), 0x00.toByte(),                //  Sony Camera
+                        0x64.toByte(), 0x00.toByte(),                //  Version
+                        0x00.toByte(), 0x00.toByte(),                //  Model code, as utf8
+                        0x22.toByte(), 0xC0.toByte(), 0x00.toByte(), //  0x80 = pairing supported, 0x40 = pairing enabled, 0x20 = location supported, 0x10 = location enabled, 0x8 = remote supported, 0x4 = remote enabled ( in 6600)
+                        // present in other protocol version ?
+                        //0x23.toByte(), 0x00.toByte(),0x00.toByte(), // 0x80 = remote control supported, 0x20 = remote control enabled, 0x10 = image transfer supported , 0x04 = image transfer enabled 0x2 = push transfer supported, 0x01 = push transfer enabled
+                        //0x21.toByte() 0x00.toByte(),00.toByte() // 0x80 = wirless power on enabled, 0x40 = camera on, 0x20 = wifi handover supported, 0x10 = wifi handover enabled
                     ),
-                    byteArrayOf(            //Filter bit mask
-                        0xff.toByte(), 0xff.toByte(),               //Camera
-                        0x00.toByte(),                              //Protocol version. For now we will ignore this as older models like the a6400 report 0x64 and models from around 2023 report 0x65. No idea what the differences are and when compatibility will break, but so far there does not seem to be a camera with Bluetooth support that is not compatible with the standard BLE remote from Sony. So, for now this seems to be fine.
-                        0x00.toByte(),                              //Unknown
-                        0x00.toByte(), 0x00.toByte(),               //Model
-                        0xff.toByte(), 0x40.toByte(), 0x00.toByte() //Camera state, bit 0x40,  indicates ready to pair
+                    byteArrayOf(              // Filter bit mask
+                        0xff.toByte(), 0xff.toByte(),               // must have the camera value
+                        0x00.toByte(), 0x00.toByte(),               // we don't care about protocol version
+                        0x00.toByte(), 0x00.toByte(),               // we don't care about model code
+                        0xff.toByte(), 0xC0.toByte(), 0x00.toByte() // require pairing enable and supported
                     )
-                    //TODO: We might be able to check if the remote control feature is enabled on the camera here.
-                    //Coral claims that 0x02 of the 8th byte indicates the remote control feature.
-                    //(see https://github.com/coral/freemote/blob/14cfde8dcf6576b4fad6c2283c19c650c44f8e61/src/BLECamera.cpp#L237)
-                    //However, my a6400 advertises 0x03006400453122ec00... with the remote enabled and
-                    //with remote disabled it is   0x03006400453122e800...
-                    //This would suggest that we have to check for 0x04.
-                    //So, until we can verify this on a few different models, we do not check this bit to ensure compatibility
                 )
                 .build()
             )
@@ -60,10 +55,10 @@ object CompanionDeviceHelper {
             .setSingleDevice(true)
             .build()
 
-        val deviceManager = context.getSystemService(Context.COMPANION_DEVICE_SERVICE) as CompanionDeviceManager
+        val deviceManager = ContextCompat.getSystemService(context,CompanionDeviceManager::class.java)
 
         Log.d(MainActivity.TAG, "Associating.")
-        deviceManager.associate(associationRequest, callback, null)
+        deviceManager?.associate(associationRequest, callback, null)
     }
 
     fun startObservingDevicePresence(context: Context, device: BluetoothDevice): Boolean {
@@ -84,7 +79,8 @@ object CompanionDeviceHelper {
     }
 
     fun unpairCompanionDevice(context: Context) {
-        val deviceManager = context.getSystemService(Context.COMPANION_DEVICE_SERVICE) as CompanionDeviceManager
+        val deviceManager =
+            ContextCompat.getSystemService(context, CompanionDeviceManager::class.java) ?: return
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA) {
             for (association in deviceManager.myAssociations) {
