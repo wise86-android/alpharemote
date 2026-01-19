efdd24fe03ead4bccb1bcc10b120ec282e51cee4
wise <wise86android@gmail.com>
2026-01-19 22:06:09 +0100

    feat: use agp9 feature, remove deprecate code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,5 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.compose)
 }
 
@@ -34,13 +30,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
-    kotlin {
-        compilerOptions {
-            jvmTarget = JvmTarget.fromTarget("11")
-        }
-    }
-
 
     buildFeatures {
         viewBinding = true
